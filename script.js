1666cdfa78232379afe41575815aef8321d926fa
igorgvozden <igorgvozden@yahoo.com>
2020-06-27 22:19:09 +0200

    added services active class and content change on buttons clicks

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let hamburgerBtn = document.querySelector(".nav__item__button--ham");
 let closeBtn = document.querySelector(".nav__item__button--x");
 let menu = document.querySelector(".page-container__right");
 
+// this toggles menu view 
+
 hamburgerBtn.addEventListener("click", () => {
     hamburgerBtn.style.display = "none";
     menu.style.width = "290px";
@@ -12,4 +14,21 @@ closeBtn.addEventListener("click", () => {
     closeBtn.style.display = "none";
     menu.style.width = "0px";
     hamburgerBtn.style.display = "block";
+});
+
+// services section -- this puts active link class on selected service
+
+let serviceButtons = document.querySelectorAll(".services-container__box__button");
+let serviceTitle = document.querySelector(".services-info__container__title");
+
+function removeActiveClass() {
+    serviceButtons.forEach(el => el.classList.remove("active-service"));
+};
+
+serviceButtons.forEach(el => {
+    el.addEventListener("click", (e) => {
+        removeActiveClass();
+        e.target.classList.add("active-service");
+        serviceTitle.innerHTML = `${e.target.id}<span> services</span>`;
+    })
 });
