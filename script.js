940b20a91a1fc059fcd60b6c68f106c1519afcc2
igorgvozden <igorgvozden@yahoo.com>
2020-06-28 14:43:41 +0200

    mission section added

--- script.js
+++ script.js
@@ -22,13 +22,22 @@ let serviceButtons = document.querySelectorAll(".services-container__box__button
 let serviceTitle = document.querySelector(".services-info__container__title");
 
 function removeActiveClass() {
-    serviceButtons.forEach(el => el.classList.remove("active-service"));
+    serviceButtons.forEach(el => {
+        el.classList.remove("active-service");
+        el.innerHTML = "learn more";
+    });
 };
 
 serviceButtons.forEach(el => {
     el.addEventListener("click", (e) => {
         removeActiveClass();
         e.target.classList.add("active-service");
+        e.target.innerHTML = "see below";
         serviceTitle.innerHTML = `${e.target.id}<span> services</span>`;
     })
 });
+
+window.onload = () => {
+    serviceButtons[0].classList.add("active-service");
+    serviceButtons[0].innerHTML = "see below";
+};
