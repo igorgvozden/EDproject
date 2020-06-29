349270676bce26ad9e990dd5e5f0acd21f0164b6
igorgvozden <igorgvozden@yahoo.com>
2020-06-29 11:50:04 +0200

    socialize with us section with functionality added

--- script.js
+++ script.js
@@ -60,14 +60,41 @@ function validateForm() {
     let lastName = document.getElementById("last-name");
     let informator = document.querySelector(".contact-container__form-container__informator");
 
+    informator.innerHTML = "";
+
+    let validator = true; 
+    let informations = [];
+
     if (firstName.value.length < 3) {
-        informator.innerHTML = "First Name must have at least 3 characters";
-        return false;
+        informations.push("First Name must have at least 3 characters");
+        validator = false;
     }
 
     if (lastName.value.length < 3) {
-        informator.innerHTML = "First Name must have at least 3 characters";
-        return false;
+        informations.push("Last Name must have at least 3 characters");
+        validator = false;
     }
-    return true;
+    
+    if (informations.length >= 1) {
+        informations.forEach(el => {
+        let paragh = document.createElement("P");
+        let nodeText = document.createTextNode(el);
+        paragh.appendChild(nodeText);
+        
+        informator.appendChild(paragh);
+        })
+    }
+    
+    return validator;
 };
+
+// social section
+
+let socialTextJson = `[{"count":"21", "text":"This is awesome"}, {"count":3, "text":"Take care of what you eat. You are what you eat!"}, {"count":"15", "text":"Are your Zzz's beeing affected by stress? Sleeping eight hours every night reduces stress, makes you more alert, and helps your immune response. Get better sleep by turning off all blue light devices two hours before bed, exercising, lowering the temperature in your bedroom, and taking Melatonin. Have a restful weekend, everyone!"}, {"count":"16", "text":"Sun bathing can have many benefits for your health, from boosting immune system to looking nice and tanned during summer days"}, {"count":"41", "text":"Turn your Zen mode on! According to HO, living space can stage your performance and mental health"}]`;
+let socialText = JSON.parse(socialTextJson);
+
+let socialLikes = document.querySelectorAll(".social__container__box--hidden-likes");
+let socialMessage = document.querySelectorAll(".social__container__box--hidden-text");
+
+socialLikes.forEach((el, i) => el.appendChild(document.createTextNode(socialText[i].count)));
+socialMessage.forEach((el, i) => el.appendChild(document.createTextNode(socialText[i].text)));
